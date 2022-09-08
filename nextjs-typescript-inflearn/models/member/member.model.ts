9f82cc7e2ec91e35bf218d980c6f144b69dc91ab
Geun-Oh <kandy1002@naver.com>
2022-09-08 15:02:31 +0000

    MemberModel description updated

--- nextjs-typescript-inflearn/models/member/member.model.ts
+++ nextjs-typescript-inflearn/models/member/member.model.ts
@@ -1,7 +1,14 @@
 import FirebaseAdmin from "../firebase_admin";
 import { InAuthUser } from "../in_auth_user";
+/**
+ * 유저를 추가하는 로직이 구현된 모델이다.
+ * add함수에 uid, email, displayName, photoURL을 보내면
+ * 이를 firebase에 유저 데이터로 추가하여 준다.
+ */
 
-/**변경될 수 있는 변수들을 상수화한다. API콜과 같은 부분을 따로 빼서 여기에 저장한다. */
+
+/*변경될 수 있는 변수들을 상수화한다. API콜과 같은 부분을 따로 빼서 여기에 저장한다. */
+
 const MEMBER_COL = "members";
 const SCR_NAME_COL = "screen_names";
 
@@ -10,7 +17,7 @@ type AddResult = { result: true; id: string } | { result: false; message: string
 async function add({ uid, email, displayName, photoURL }: InAuthUser): Promise<AddResult> {
   try {
     const addResult = await FirebaseAdmin.getInstance()
-      .Firebase.collection("members")
+      .Firebase.collection(MEMBER_COL)
       .doc(uid)
       .set({
         uid,
@@ -20,7 +27,7 @@ async function add({ uid, email, displayName, photoURL }: InAuthUser): Promise<A
       });
     const screenName = (email as string).replace("@gmail.com", "");
     await FirebaseAdmin.getInstance()
-      .Firebase.collection("screen_names")
+      .Firebase.collection(SCR_NAME_COL)
       .doc(screenName)
       .set({
         uid,
@@ -28,13 +35,17 @@ async function add({ uid, email, displayName, photoURL }: InAuthUser): Promise<A
         displayName: displayName ?? "",
         photoURL: photoURL ?? "",
       });
+    /**
+     * 데이터베이스에 대해서 트랜젝션을 실행한다.
+     * 즉 데이터를 변경하는 과정을 말하는 것!(추가, 삭제를 포함해서)
+    */      
     await FirebaseAdmin.getInstance().Firebase.runTransaction(
       async (transaction) => {
         const memberRef = FirebaseAdmin.getInstance()
-          .Firebase.collection("members")
+          .Firebase.collection(MEMBER_COL)
           .doc(uid);
         const screenNameRef = FirebaseAdmin.getInstance()
-          .Firebase.collection("screen_names")
+          .Firebase.collection(SCR_NAME_COL)
           .doc(screenName);
         const memberDoc = await transaction.get(memberRef);
         if (memberDoc.exists) {
