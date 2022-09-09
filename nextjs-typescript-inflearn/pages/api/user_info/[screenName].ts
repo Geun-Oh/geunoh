1b10dad102baf78d5eec43ceca58f16f410e0648
Geun-Oh <kandy1002@naver.com>
2022-09-09 13:30:33 +0000

    Member Model user info calling api updated

--- nextjs-typescript-inflearn/pages/api/user_info/[screenName].ts
+++ nextjs-typescript-inflearn/pages/api/user_info/[screenName].ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import checkSupportMethod from "../../controller/error/check_support_method";
-import handleError from "../../controller/error/handle_error";
-import MemberCtrl from "../../controller/member.ctrl";
+import checkSupportMethod from "../../../controller/error/check_support_method";
+import handleError from "../../../controller/error/handle_error";
+import MemberCtrl from "../../../controller/member.ctrl";
 
 /**
  * 앞으로 우리가 처리하는 api들은 대부분 이러한 형태를 띌 것이므로 그 구조를 잘 알아두기!
@@ -17,10 +17,9 @@ export default async function handler(
   const supportMethod = ["POST"];
   try {
     checkSupportMethod(supportMethod, method!);
-    await MemberCtrl.add(req, res);
+    await MemberCtrl.findByScreenName(req, res);
   } catch (err) {
     console.error(err)
     handleError(req, res)
   }
 }
-
