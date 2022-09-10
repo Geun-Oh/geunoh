f705b468d452a34c8331e31bb79a019fd05b7c05
Geun-Oh <kandy1002@naver.com>
2022-09-10 01:05:25 +0000

    effective typescript item9 completed

--- effective-typescript/src/index.ts
+++ effective-typescript/src/index.ts
@@ -1,34 +1,6 @@
 interface Person {
-    first: string;
-    last: string;
+    name: string;
 }
 
-const p: Person = { first: "Jane", last: "Jacobs" };
-
-const email = (p: Person, subject: string, body: string): Response => {
-    return new Response()
-}
-
-class Cylinder {
-    radius = 1;
-    height = 1;
-}
-
-function calculateVolume(shape: unknown) {
-    if(shape instanceof Cylinder) {
-        shape; // It's type is Cylinder!
-        shape.radius; // It's type is number!
-    }
-}
-
-type t1 = typeof p
-type t2 = typeof email
-
-type t = typeof Cylinder
-const type = typeof Cylinder
-type v = InstanceType<typeof Cylinder>
-
-console.log(type)
-
-const v1 = typeof p
-const v2 = typeof email
+const body = document.body;
+const el = body as unknown as Person;
