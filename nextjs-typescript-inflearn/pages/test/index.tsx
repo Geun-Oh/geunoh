464d3a8e33782493f0d901fdcf52aef8e4a143b4
Geun-Oh <kandy1002@naver.com>
2022-09-12 10:07:49 +0000

    22.09.12 commit

--- nextjs-typescript-inflearn/pages/test/index.tsx
+++ nextjs-typescript-inflearn/pages/test/index.tsx
@@ -1,21 +1,43 @@
 import axios, { AxiosPromise } from "axios";
-import { NextPage } from "next";
-import { useEffect, useState } from "react";
-import { json } from "stream/consumers";
+import { GetServerSideProps, NextPage } from "next";
+import { useEffect } from "react";
+import { InAuthUser } from "../../models/in_auth_user";
 
-const Page: NextPage = () => {
-    useEffect(() => {
-        async function getRes() {
-            try {
-                const d: AxiosPromise<any> = await axios(`https://3000-geunoh-geunoh-e6igz5vdugx.ws-us64.gitpod.io/api/user_info/a01091634257`).then(res => res.data)
-                console.log(d);
-            } catch (err) {
-                console.log(err);
+interface Props {
+    data: InAuthUser | null;
+}
+
+const Page: NextPage<Props> = ({ data }) => {
+    // useEffect(() => {
+    //     async function getRes() {
+    //         try {
+    //             const d: AxiosPromise<any> = await axios(`https://3000-geunoh-geunoh-e6igz5vdugx.ws-us64.gitpod.io/api/user_info/a01091634257`).then(res => res.data)
+    //             console.log(d);
+    //         } catch (err) {
+    //             console.log(err);
+    //         }
+    //     }
+    //     getRes();
+    // }, [])
+    return <div>{data?.displayName}</div>;
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+    try {
+        const resData: InAuthUser | null = await fetch('https://3000-geunoh-geunoh-e6igz5vdugx.ws-us64.gitpod.io/api/user_info/a01091634257').then(res => console.info(res));
+        return {
+            props: {
+                data: resData,
             }
         }
-        getRes();
-    }, [])
-    return <div>hello!</div>;
+    } catch (err) {
+        console.error(err);
+        return {
+            props: {
+                data: null,
+            }
+        }
+    }
 }
 
 export default Page;
