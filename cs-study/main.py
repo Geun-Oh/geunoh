6a2f18c5447e4efb62b831b5a3f0dab86e78a2eb
Geun-Oh <kandy1002@naver.com>
2022-11-21 14:47:42 +0000

    파이썬으로 백준 풀기 쉽지 않다...

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,22 +1,41 @@
-node = int(input())
-lineNum = int(input())
-ans = []
-line = [[] for _ in range(node + 1)]
-for _ in range(lineNum):
-    i = list(map(int, input().split()))
-    if i[1] not in line[i[0]]:
-        line[i[0]].append(i[1])
-    if i[0] not in line[i[1]]:
-        line[i[1]].append(i[0])
+from collections import deque
 
-visited = [False] * (node + 1)
+des = list(map(int, input().split()))
+m = [['0' for i in range(des[1] + 2)]]
+count = 0
+steps = [[-1, 0], [0, 1], [1, 0], [0, -1]]
+for _ in range(des[0]):
+    t = list(input())
+    t.insert(0, '0')
+    t.append('0')
+    m.append(t)
+m.append(['0' for i in range(des[1] + 2)])
+visited = [[False for _ in range(des[1] + 2)] for _ in range(des[0] + 2)]
 
-def dfs(line, v, visited):
-    visited[v] = True
-    ans.append(v)
-    for i in line[v]:
-        if not visited[i]:
-            dfs(line, i, visited)
-
-dfs(line, 1, visited)
-print(len(ans) - 1)
+def bfs(map, now, visited):
+    queue = deque()
+    queue.append(now)
+    newmap = [[0 for _ in range(des[1] + 2)] for _ in range(des[0] + 2)]
+    newmap[1][1] += 1
+    while queue:
+        global count
+        count += 1
+        visited[now[0]][now[1]] = True
+        n = queue.popleft()
+        if n == des:
+            print(count, newmap[des[0]][des[1]])
+            return
+        if visited[n[0] + steps[0][0]][n[1] + steps[0][1]] == False and m[n[0] + steps[0][0]][n[1] + steps[0][1]] == '1':
+            queue.append([n[0] + steps[0][0], n[1] + steps[0][1]])
+            newmap[n[0] + steps[0][0]][n[1] + steps[0][1]] = newmap[n[0]][n[1]]
+        if visited[n[0] + steps[1][0]][n[1] + steps[1][1]] == False and m[n[0] + steps[1][0]][n[1] + steps[1][1]] == '1':
+            queue.append([n[0] + steps[1][0], n[1] + steps[1][1]])
+            newmap[n[0] + steps[1][0]][n[1] + steps[1][1]] = newmap[n[0]][n[1]]
+        if visited[n[0] + steps[2][0]][n[1] + steps[2][1]] == False and m[n[0] + steps[2][0]][n[1] + steps[2][1]] == '1':
+            queue.append([n[0] + steps[2][0], n[1] + steps[2][1]])
+            newmap[n[0] + steps[2][0]][n[1] + steps[2][1]] = newmap[n[0]][n[1]]
+        if visited[n[0] + steps[3][0]][n[1] + steps[3][1]] == False and m[n[0] + steps[3][0]][n[1] + steps[3][1]] == '1':
+            queue.append([n[0] + steps[3][0], n[1] + steps[3][1]])
+            newmap[n[0] + steps[3][0]][n[1] + steps[3][1]] = newmap[n[0]][n[1]]
+        
+bfs(m, [1, 1], visited)
