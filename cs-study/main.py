f6986b46b1c743d61392e8ed223a0ae427cfcaeb
Geun-Oh <kandy1002@naver.com>
2022-11-27 03:57:58 +0000

    BOJ 2468 solving...

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,43 +1,49 @@
 from collections import deque
 
-width, height = list(map(int, input().split()))
+height = int(input())
 
 graph = []
 for i in range(height):
     t = list(map(int, input().split()))
     graph.append(t)
 
-def bfs(graph):
+for i in range(height):
+    for j in range(height):
+        if graph[i][j] <= height:
+            graph[i][j] = 0
+        elif graph[i][j] > height:
+            graph[i][j] = 1
+    
+def bfs(x, y, graph):
     queue = deque()
-    for i in range(width):
-        for j in range(height):
-            if graph[j][i] == 1:
-                queue.append((i + 1, j, i, j))
-                queue.append((i - 1, j, i, j))
-                queue.append((i, j + 1, i, j))
-                queue.append((i, j - 1, i, j))
-    while queue:
-        a, b, la, lb = queue.popleft()
-        if a <= -1 or a >= width or b <= -1 or b >= height:
-            continue
-        if graph[b][a] == -1:
-            continue
-        if graph[b][a] == 0:
-            graph[b][a] = graph[lb][la] + 1
-            queue.append((a + 1, b, a, b))
-            queue.append((a - 1, b, a, b))
-            queue.append((a, b + 1, a, b))
-            queue.append((a, b - 1, a, b))
-        
-    # graph 내에 0이 있으면 -1을 출력하고,
-    # 없으면 최대값 - 1 을 출력한다.
-    n = []
-    for i in range(height):
-        for j in range(width):
-            if graph[i][j] == 0:
-                print(-1)
-                return
-        n.append(max(graph[i]))
-    print(max(n) - 1)
+    if x <= -1 or x >= height or y <= -1 or x >= height:
+        return False
+    if graph[x][y] == 0:
+        return False
+    if graph[x][y] == 1:
+        graph[x][y] = 0
+        queue.append((x + 1, y))
+        queue.append((x - 1, y))
+        queue.append((x, y + 1))
+        queue.append((x, y - 1))
+        while queue:
+            a, b = queue.popleft()
+            if a <= -1 or a >= height or b <= -1 or b >= height:
+                continue
+            if graph[a][b] == 0:
+                continue
+            if graph[a][b] == 1:
+                graph[a][b] = 0
+                queue.append((a + 1, b))
+                queue.append((a - 1, b))
+                queue.append((a, b + 1))
+                queue.append((a, b - 1))
+        return True
 
-bfs(graph)
+count = 0
+for i in range(height):
+    for j in range(height):
+        if bfs(i, j, graph) == True:
+            count += 1
+        
+print(count)
