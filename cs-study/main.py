ce4b1df0a132ab8d3ba8b615cd8d8e006e299102
Geun-Oh <kandy1002@naver.com>
2022-11-27 13:15:27 +0000

    solved.ac class 1 achieved

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,26 +1,9 @@
-from collections import Counter
-
 n = int(input())
 
-arr = []
+arr = list(map(int, input().split()))
+m = max(arr)
 
 for i in range(n):
-    arr.append(int(input()))
+    arr[i] = arr[i] / m * 100
 
-c = Counter(arr).most_common()
-most = []
-for i in c:
-    if i[1] == c[0][1]:
-        most.append(i)
-choi = 0
-if len(most) > 1:
-    most.sort()
-    choi = most[1][0]
-else:
-    choi = most[0][0]
-arr.sort()
-
-print(round(sum(arr) / len(arr)))
-print(arr[int((len(arr) + 1) / 2 - 1)])
-print(choi)
-print(arr[-1] - arr[0])
+print(sum(arr) / n)
