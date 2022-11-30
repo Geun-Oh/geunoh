3385df6dbf6f75e3d58dfd126cd2b31bb6def894
Geun-Oh <kandy1002@naver.com>
2022-11-30 11:41:17 +0000

    solving 15650 with python...

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,30 +1,19 @@
-from collections import deque
+n, m = list(map(int, input().split()))
 
-n, m = list(map(int, input().split())) # 사람 수, 파티 수
-known = list(map(int, input().split()))
+check = []
 
-def solve(n, m, known):
-    if len(known) == 1:
-        print(m)
+def dfs(arr):
+    if len(arr) >= m:
         return
-    known.pop(0)
-    parties = []
-    queue = deque()
-    for i in known:
-        queue.append(i)
-    for i in range(m):
-        t = list(map(int, input().split()))
-        k = t.pop(0)
-        parties.append((k, t))
-    count = [1] * m
-    while queue:
-        know = queue.popleft()
-        for party in parties: # 알고 있는 사람들이 known
-            if know in party[1]:
-                i = parties.index(party)
-                knownparty = parties.pop(i)
-                for knownpeople in knownparty[1]:
-                    queue.append(knownpeople)
-    print(len(parties))
-            
-solve(n, m, known)
+    for i in range(n):
+        arr.append(i + 1)
+        if arr in check:
+            continue
+        else:
+            print(arr)
+            check.append(arr)
+            dfs(arr)
+
+dfs([])
+
+print(check)
