814aef198e2528fe444608ce9707ff49c302ced0
Geun-Oh <kandy1002@naver.com>
2022-12-28 13:37:56 +0000

    solved BOJ 1629 with python

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,34 +1,16 @@
-from collections import deque
+import sys
 
-n = int(input())
-nodes = list(map(int, input().split()))
+input = sys.stdin.readline
+a, b, c = map(int, input().split())
 
-tree = [[] for i in range(n)]
-
-def bfs():
+def sol(n):
     if n == 1:
-        print(nodes[0])
-        return
-    queue = deque()
-    center = nodes[(len(nodes) + 1) // 2 - 1]
-    tree[0].append(center)
-    queue.append((nodes[:(len(nodes) + 1) // 2 - 1], nodes[(len(nodes) + 1) // 2:]))
-    depth = 1
-    while queue:
-        items = queue.popleft()
-        if len(items[0]) == 1:
-            for item in items:
-                tree[depth].append(item[0])
-                print(depth, tree)
-            continue
-        for item in items:
-            cen = item[(len(item) + 1) // 2 - 1]
-            tree[depth].append(cen)
-            queue.append((item[:(len(item) + 1) // 2 - 1], item[(len(item) + 1) // 2:]))
-            print(depth, tree)
-        depth += 1
+        return a % c
+    else:
+        temp = sol(n // 2)
+        if n % 2 == 0:
+            return (temp * temp) % c
+        else:
+            return (temp * temp * a) % c
 
-    for i in tree:
-        print(*i)
-
-bfs()
+print(sol(b))
