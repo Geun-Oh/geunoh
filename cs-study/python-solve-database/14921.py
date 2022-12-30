5a56ca98bf513a40b610eec4af8a7a3fe5ce5572
Geun-Oh <kandy1002@naver.com>
2022-12-30 16:09:23 +0000

    solved BOJ 1253 with python

---
+++ cs-study/python-solve-database/14921.py
@@ -0,0 +1,25 @@
+import sys
+
+n = int(input())
+arr = list(map(int, sys.stdin.readline().split()))
+
+l = 0
+r = n - 1
+s = 9876543210
+ans = [0, n - 1]
+
+while l < r:
+    nows = arr[l] + arr[r]
+    if abs(nows) < abs(s):
+        s = nows
+        ans[0] = l
+        ans[1] = r
+
+    if nows < 0:
+        l += 1
+    elif nows > 0:
+        r -= 1
+    else:
+        break
+
+print(arr[ans[0]] + arr[ans[1]])
