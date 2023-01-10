09a7d2e3c1bbe1babd84ff1efc211312e597b651
Geun-Oh <kandy1002@naver.com>
2023-01-10 14:10:04 +0000

    solved BOJ 2110 with python

--- cs-study/python-solve-database/2110.py
+++ cs-study/python-solve-database/2110.py
@@ -29,21 +29,3 @@ def sol():
 
 sol()
 print(answer)
-
-
-# n, m = map(int, input().split())
-# arr = sorted(list(map(int, input().split())))
-
-# l = 0
-# r = n - 1
-# now = (l + r) // 2
-
-# while l >= r:
-#     s = 0
-#     for i in arr:
-#         if i > arr[now]:
-#             s += (i - arr[now])
-#     if s >= m:
-
-#         break
-#     if s < m:
