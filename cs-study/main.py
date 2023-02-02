e01d02bc717f3e99ac58ec6e3ca27fd80376081d
Geun-Oh <kandy1002@naver.com>
2023-02-02 14:36:38 +0000

    solving BOJ 2630 with python...

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,66 +1,23 @@
 import sys
-import copy
-from collections import deque
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n = int(input())
 
 arr = []
 for i in range(n):
-    arr.append(list(map(int, input().split())))
-
-virus = []
-empty = []
-for i in range(n):
-    for j in range(m):
-        if arr[i][j] == 0:
-            empty.append([i, j])
-        elif arr[i][j] == 2:
-            virus.append((i, j))
-
-def bfs(arr):
-    for a in virus:
-        queue = deque()
-        y, x = a
-        queue.append((y + 1, x))
-        queue.append((y - 1, x))
-        queue.append((y, x + 1))
-        queue.append((y, x - 1))
-        while queue:
-            a, b = queue.popleft()
-            if a <= -1 or a >= n or b <= -1 or b >= m:
-                continue
-            if arr[a][b] != 0:
-                continue
-            arr[a][b] = 2
-            queue.append((a + 1, b))
-            queue.append((a - 1, b))
-            queue.append((a, b + 1))
-            queue.append((a, b - 1))
-
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if arr[i][j] == 0:
-                count += 1
-    
-    return count
+    arr.append(list(map, int(input().split()))
 
 ans = 0
 
-def solve(arr, i, j, k):
-    narr = copy.deepcopy(arr)
-    narr[empty[i][0]][empty[i][1]] = 1
-    narr[empty[j][0]][empty[j][1]] = 1
-    narr[empty[k][0]][empty[k][1]] = 1
-    cnt = bfs(narr)
-    global ans
-    if ans < cnt:
-        ans = cnt
-        
-for i in range(len(empty) - 2):
-    for j in range(i + 1, len(empty) - 1):
-        for k in range(j + 1, len(empty)):
-            solve(arr, i, j, k)
-
-print(ans)
+def solve(start, end):
+    if start == end:
+        ans += 1
+        return
+    standard = arr[start[0]][start[1]]
+    for i in range(start[0], end[0] + 1):
+        for j in range(start[1], end[1] + 1):
+            if arr[i][j] != standard:
+                solve(start, [((start[0] - 1) + end[0]) // 2, ((start[1] - 1) + end[1]) // 2])
+                solve([start[0], ((start[0] - 1) + end[0]) // 2 + 1], [((start[0] - 1) + end[0]) // 2, end[1]])
+                solve([start[0], ((start[0] - 1) + end[0]) // 2], [])
+                solve(start, [((start[0] - 1) + end[0]) // 2, ((start[1] - 1) + end[1]) // 2])
