5a40760826fbbf5b5237e82adcb2f812d17e392b
Geun-Oh <kandy1002@naver.com>
2023-02-21 23:15:38 +0900

    [level 2] Title: 모음 사전, Time: 0.95 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
+++ 프로그래머스/lv2/84512. 모음 사전/모음 사전.py
@@ -1,12 +1,7 @@
-# 중복을 허용하고 순서가 없으니 조합을 쓴다.
 from itertools import product
 
 def solution(word):
     answer = 0
-    # if len(word) < 5:
-    #     for i in range(5 - len(word)):
-    #         word += "A"
-    
     arr = []
     for i in range(1, 6):
         t = list(product(['A', 'E', 'I', 'O', 'U'], repeat = i))
