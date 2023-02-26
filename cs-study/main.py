1d9bb4680c22fac1eed45fec798c7207a11ee223
Geun-Oh <kandy1002@naver.com>
2023-02-26 14:26:01 +0000

    solving BOJ 2206 with python

--- cs-study/main.py
+++ cs-study/main.py
@@ -1,89 +1,55 @@
-from itertools import combinations
 from collections import deque
-import sys
-input = sys.stdin.readline
-case = int(input())
 
-def sol():
-    group = {'A': {}, 'B': {}}
-    node, line = list(map(int, input().split()))
-    if node == 2 or node == 1:
-        return "YES"
-    graph = [[] for i in range(node + 1)]
-    for i in range(line):
-        a, b = list(map(int, input().split()))
-        graph[a].append(b)
-        graph[b].append(a)
+n, m = list(map(int, input().split()))
+graph = []
+for i in range(n):
+    graph.append(list(map(int, input())))
+
+walls = []
+for i in range(n):
+    for j in range(m):
+        if graph[i][j] == 1:
+            walls.append((i, j))
+
+def bfs(graph, i):
+    realcnt = 1000001
+    visited = [[False] * m for i in range(n)]
     queue = deque()
-
-    visited = [False] * (node + 1)
-    for i in range(1, node + 1):
-        queue = deque()
-        visited[i] = True
-        if i not in group['A'].keys() or i not in group['B'].keys():
-            group['A'][i] = 0
-        if len(graph[i]) == 0:
+    queue.append((0, 0, 1))
+    while queue:
+        y, x, cnt = queue.popleft()
+        if y <= -1 or y >= n or x <= -1 or x >= m:
             continue
-        for j in graph[i]:
-            if i in group['A'].keys():
-                queue.append((j, 'B'))
-            if i in group['B'].keys():
-                queue.append((j, 'A'))
-
-        while queue:
-            x, g = queue.popleft()
-            if visited[x] == True:
+        if [y, x] == [n - 1, m - 1]:
+            if cnt < realcnt:
+                realcnt = cnt
+            continue
+        if graph[y][x] == 1:
+            if [y, x] != [i[0], i[1]]:
                 continue
-            visited[x] = True
-            if g == 'A':
-                if x in group['B'].keys():
-                    return "NO"
-            if g == 'B':
-                if x in group['A'].keys():
-                    return "NO"
-            group[g][x] = 0
-            for k in graph[x]:
-                if k in group[g].keys():
-                    return "NO"
-                else:
-                    if g == 'A':
-                        queue.append((k, 'B'))
-                    else:
-                        queue.append((k, 'A'))
-    return "YES"
+        if visited[y][x] == True:
+            continue
+        visited[y][x] = True
+        queue.append((y + 1, x, cnt + 1))
+        queue.append((y - 1, x, cnt + 1))
+        queue.append((y, x + 1, cnt + 1))
+        queue.append((y, x - 1, cnt + 1))
+    return realcnt
 
-for _ in range(case):
-    print(sol())
+ans = 1000001
 
-# for _ in range(case):
-#     ans = "NO"
-#     node, line = list(map(int, input().split()))
-#     graph = [[] for i in range(node + 1)]
-#     for i in range(line):
-#         a, b = list(map(int, input().split()))
-#         graph[a].append(b)
-#         graph[b].append(a)
-#     combs = []
-#     for p in range(1, (node + 1) // 2 + 1):
-#         combs = list(combinations([i for i in range(1, node + 1)], p))
-#         for a in combs:
-#             queue = deque()
-#             rev = [i for i in range(1, node + 1) if i not in a]
-#             if isUF(a, graph) == True and isUF(rev, graph) == True:
-#                 ans = "YES"
-#     print(ans)
+if len(walls) != 0:
+    for i in range(len(walls)):
+        t = bfs(graph, walls[i])
+        if t < ans:
+            ans = t
 
-# # def isUF(arr, graph):
-# #     queue = deque()
-# #     dic = {}
-# #     for mom in arr:
-# #         for son in graph[mom]:
-# #             queue.append(son)
-# #     while queue:
-# #         s = queue.popleft()
-# #         if s in dic.keys():
-# #             continue
-# #         if s in arr:
-# #             return False
-# #         dic[s] = 0
-# #     return True
+s = bfs(graph, (-1, -1))
+if s < ans:
+    ans = s
+
+if ans == 1000001:
+    ans = -1
+if n == 1 and m == 1:
+    ans = 1
+print(ans)
