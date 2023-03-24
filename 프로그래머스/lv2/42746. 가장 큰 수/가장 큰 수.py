bcd61af5d4f1aa9061736edd94e8a4e9deebda74
Geun-Oh <kandy1002@naver.com>
2023-03-24 13:43:50 +0000

    delete: deleted ps history folder

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++
@@ -1,6 +0,0 @@
-def solution(numbers):
-    numbers.sort(reverse = True, key = lambda x: str(x) * 3)
-    if set(numbers) == {0}:
-        return "0"
-    answer = ''.join(list(map(str, numbers)))
-    return answer
